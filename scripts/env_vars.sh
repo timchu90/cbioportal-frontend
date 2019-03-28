d243d1cc037ba73781a0276ad40c86d39902f8ce
Pim van Nierop <pim@thehyve.nl>
2019-03-28 15:47:30 +0100

    Correct revert back to rc [PR feedback Adam]

--- scripts/env_vars.sh
+++ scripts/env_vars.sh
@@ -5,28 +5,16 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 if [[ "$CIRCLECI" ]]; then
-
-    if [[ "$E2E_LOCALDB" = true ]]; then
-        # When on circle ci and using dockers that host a custom cbioportal
-        # portal and database use env vars that point to this docker.
-        # For local database tests evaluation of PR branch is perfored by
-        # the get_pullrequest_info.py script.
-        BACKEND=e2e_localdb
+    # on circle ci determine env variables based on branch or in case of PR
+    # what branch the PR is pointing to
+    if [[ "$CIRCLE_PR_NUMBER" ]] && ! [[ $CIRCLE_BRANCH == "release-"* ]]; then
+        BRANCH=$(curl "https://github.com/cBioPortal/cbioportal-frontend/pull/${CIRCLE_PR_NUMBER}" | grep -oE 'title="cBioPortal/cbioportal-frontend:[^"]*' | cut -d: -f2 | head -1)
+    elif [[ "$CIRCLE_PULL_REQUEST" ]] && ! [[ $CIRCLE_BRANCH == "release-"* ]]; then
+        BRANCH=$(curl "${CIRCLE_PULL_REQUEST}" | grep -oE 'title="cBioPortal/cbioportal-frontend:[^"]*' | cut -d: -f2 | head -1)
     else
-        # on circle ci determine env variables based on branch or in case of PR
-        # what branch the PR is pointing to
-        if [[ "$CIRCLE_PR_NUMBER" ]] && ! [[ $CIRCLE_BRANCH == "release-"* ]]; then
-            BRANCH=$(curl "https://github.com/cBioPortal/cbioportal-frontend/pull/${CIRCLE_PR_NUMBER}" | grep -oE 'title="cBioPortal/cbioportal-frontend:[^"]*' | cut -d: -f2 | head -1)
-        elif [[ "$CIRCLE_PULL_REQUEST" ]] && ! [[ $CIRCLE_BRANCH == "release-"* ]]; then
-            BRANCH=$(curl "${CIRCLE_PULL_REQUEST}" | grep -oE 'title="cBioPortal/cbioportal-frontend:[^"]*' | cut -d: -f2 | head -1)
-        else
-            BRANCH=$CIRCLE_BRANCH
-        fi
-        # here we will set respective external backends for some of the tests (e.g. if PR is to frontend master, then use master backend)
-        BACKEND=$BRANCH
+        BRANCH=$CIRCLE_BRANCH
     fi
-
-    cat $SCRIPT_DIR/../env/${BACKEND}.sh
+    cat $SCRIPT_DIR/../env/${BRANCH}.sh
 elif [[ "$BRANCH_ENV" ]]; then
     cat $SCRIPT_DIR/../env/${BRANCH_ENV}.sh
 
