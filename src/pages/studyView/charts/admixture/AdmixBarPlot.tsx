4aa2796dbbde0708f093765685a35899378b666d
tchu <tchu@nygenome.org>
2019-05-06 16:13:09 -0400

    mostly working admixture plot

--- src/pages/studyView/charts/admixture/AdmixBarPlot.tsx
+++ src/pages/studyView/charts/admixture/AdmixBarPlot.tsx
@@ -12,9 +12,10 @@ import {STUDY_VIEW_CONFIG} from "../../StudyViewConfig";
 import {getTextDiagonal, getTextHeight, getTextWidth} from "../../../../shared/lib/wrapText";
 import {AdmixtureType} from "pages/studyView/StudyViewPageStore";
 import * as d3 from "d3"
+import {event as currentEvent} from 'd3';
 
 export interface IAdmixBarPlotProps {
-    promise: MobxPromise<AdmixtureType>;
+    promise: MobxPromise<AdmixtureType[]>;
     width: number;
     height: number;
     filters: ClinicalDataIntervalFilterValue[];
@@ -24,6 +25,7 @@ export interface IAdmixBarPlotProps {
 export default class AdmixBarPlot extends React.Component<IAdmixBarPlotProps, {}> implements AbstractChart{
     
     private svgElement: SVGElement;
+    private tooltipElement: any;
     
     constructor(props: IAdmixBarPlotProps) {
         super(props);
@@ -34,25 +36,164 @@ export default class AdmixBarPlot extends React.Component<IAdmixBarPlotProps, {}
     }
     
     public componentDidMount() {
-        const svg = d3.select("svg")
-        svg.append('text')
-            .text('hello world')
-            .attr('x', 10)
-            .attr('y',10)
-    }
-    
-    shouldComponentUpdate() {
-        return false; 
+        var svg = d3.select(this.svgElement)
+        var margin = 20
+        var width = this.props.width;
+        var height = this.props.height;
+
+        var x = d3.scale.linear().range([70,width-50]).domain([0,1000])
+        var y = d3.scale.linear().range([0,height]).domain([0,1.2])
+        var scale = d3.scale.linear().range([0,height]).domain([0,1000])
+
+        var populations = ['ADMIX_AFR','ADMIX_AMR','ADMIX_EAS','ADMIX_EUR','ADMIX_SAS']
+        var populationColors = ['#2986e2','#f88508','#dc3912','#109618','#990099']
+
+        var colorScale = d3.scale.ordinal().range(populationColors).domain(populations)
+
+        var div = d3.select(this.tooltipElement).append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0)
+            .style('position','fixed')
+            .style('text-align','left')
+            .style('width','120px')
+            .style('height','50px')
+            .style('padding-left','10px')
+            .style('padding-top','5px')
+            .style('border','0.5px solid gray')
+            .style('background','white')
+            .style('border-radius','5px')
+            .style('pointer-events','none')
+            .style('box-shadow', '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)')
+
+        var yaxis = svg.append("g")
+            .attr('class', 'yaxis')
+            .attr('transform','translate(' + x(-25) + ',' + y(0.05) + ')')
+            
+        
+        yaxis.append('line')
+            .attr('x1', 0)
+            .attr('x2', 0)
+            .attr('y1', y(0))
+            .attr('y2', y(1))
+            .attr('stroke','black')
+        
+        yaxis.append('line')
+            .attr('x1', 0)
+            .attr('x2', -5)
+            .attr('y1', y(0))
+            .attr('y2', y(0))
+            .attr('stroke','black')
+        
+        yaxis.append('line')
+            .attr('x1', 0)
+            .attr('x2', -5)
+            .attr('y1', y(0.5))
+            .attr('y2', y(0.5))
+            .attr('stroke','black')
+        
+        yaxis.append('line')
+            .attr('x1', 0)
+            .attr('x2', -5)
+            .attr('y1', y(1))
+            .attr('y2', y(1))
+            .attr('stroke','black')
+        
+        yaxis.append('text')
+            .text('1.0')
+            .attr('x', -10)
+            .attr('y', y(0))
+            .attr('text-anchor','end')
+            .attr('font-size', 12.5)
+            .attr('dy', 3)
+        
+        yaxis.append('text')
+            .text('0.5')
+            .attr('x', -10)
+            .attr('y', y(0.5))
+            .attr('text-anchor','end')
+            .attr('font-size', 12.5)
+            .attr('dy',3)
+        
+        yaxis.append('text')
+            .text('0.0')
+            .attr('x', -10)
+            .attr('y', y(1))
+            .attr('text-anchor','end')
+            .attr('font-size', 12.5)
+            .attr('dy',3)
+        
+        yaxis.append('text')
+            .text('Admixture Proportion')
+            .attr('x', -40)
+            .attr('y', y(0.5))
+            .attr('transform','rotate(270,' + -40 + ',' + y(0.5) + ')')
+            .attr('text-anchor','middle')
+            .attr('font-size', 12.5)
+            .attr('dy',scale(15))
+
+      //start drawing actual data
+      
+        var data = this.props.promise.result
+
+        var bars = svg.append('g').attr('class','bars')
+        
+        var bar = bars.selectAll('.bar')
+            .data(data).enter()
+            .append('g')
+            .attr('transform',function(d: any,i: number){
+                return 'translate(' + x(1000/data.length * i)  + ',' + y(0.05) + ')'
+            })
+        
+        for (var i in populations){
+            bar.append('rect')
+                .attr('class', populations[i])
+                .attr('width', function(){
+                    return x(1000)/data.length - 12
+                })
+                .attr('height', function(d: any) { return y(d[populations[i]])})
+                .attr('fill', colorScale(populations[i]))
+                .attr('y', function(d: any) {
+                    if (Number(i) > 0){
+                        var yshift = 0
+                        for (var j = 0; j < Number(i) ; j++){
+                            yshift = yshift + parseFloat(d[populations[j]])
+                        }
+                        return y(yshift)
+                    } else {
+                        return 0
+                    }
+                })
+                .on('mousemove',function(d: any,i: number){
+                    var population = d3.select(this).attr('class')
+                    //https://github.com/facebook/react/issues/6641#issuecomment-267365773
+                    div.html("<b>" + d.patientId + "</b>" + "<br>" +
+                            "<b>" + population + ":</b>" + d[population])
+                    .style("left", function(){
+                        return (currentEvent.clientX + 10) + "px"
+                    })
+                    .style("top", function(){
+                        return (currentEvent.clientY + 10) + "px"
+                    })
+                    .style("opacity", .9)
+                })
+                .on('mouseout',function(d: any){
+                    div.style('opacity',0)
+                })
+        }
     }
     
     public render() {
         return (
-            <svg
-                width={this.props.width}
-                height={this.props.height}
-                ref={(ref: any) => this.svgElement = ref}
-            >
-            </svg>
+            <div 
+                ref={(ref: any) => this.tooltipElement = ref}
+                >
+                <svg
+                    width={this.props.width}
+                    height={this.props.height}
+                    ref={(ref: any) => this.svgElement = ref}
+                >
+                </svg>
+            </div>
         );
     }
     
