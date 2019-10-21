8b239fbad3b19b04060be4c4697dbbe73a64407f
Pim van Nierop <pim@thehyve.nl>
2019-10-21 12:25:18 +0200

    Impl. circle-question-mark icon

--- src/shared/components/sampleLabel/SampleLabelNotProfiled.tsx
+++ src/shared/components/sampleLabel/SampleLabelNotProfiled.tsx
@@ -14,7 +14,7 @@ export default class SampleLabelNotProfiled extends React.Component<ISampleLabel
                 sample={this.props.sample}
                 extraTooltipText={'This gene was not profiled for this sample (absent from gene panel). It is unknown whether it is mutated.'} >
                 <svg width="12" height="12" data-test="not-profiled-icon">
-                    <image href={require("../../../rootImages/question-mark.png")} height="100%" width="100%" />
+                    <image href={require("../../../rootImages/circle_question_mark.png")} height="100%" width="100%" />
                 </svg>
             </SampleInline>)
         ;
